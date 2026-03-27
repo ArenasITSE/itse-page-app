8b2862abe82e22f9f35086a7a5a3679f45d59e44
ArenasITSE <EduardoArenas@escarcega.tecnm.mx>
2026-03-27 10:13:06 -0600

    agregacion de img y cuerpo del home

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Nav_barComponent } from './pages/nav_bar/nav_bar.component';
+import { FooterComponent } from "./pages/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true, // 🔥 ESTO ES LO QUE TE FALTA
   imports: [
     RouterOutlet,
-    Nav_barComponent
-  ],
+    Nav_barComponent,
+    FooterComponent
+],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

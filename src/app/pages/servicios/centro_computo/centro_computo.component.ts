2b927c1c74ce8fcc2679b75ce97e0942cb5c98ec
ArenasITSE <EduardoArenas@escarcega.tecnm.mx>
2026-04-13 15:09:34 -0600

    terminacion de modulos

--- src/app/pages/servicios/centro_computo/centro_computo.component.ts
+++ src/app/pages/servicios/centro_computo/centro_computo.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; // 👈 IMPORTANTE
 
 @Component({
   selector: 'app-centro_computo',
+  standalone: true, // 👈 si usas standalone
+  imports: [CommonModule], // 👈 AGREGA ESTO
   templateUrl: './centro_computo.component.html',
   styleUrls: ['./centro_computo.component.css']
 })
 export class Centro_computoComponent implements OnInit {
 
+  tab: string = 'sistema';
+
   constructor() { }
+  
+  ngOnInit() {}
 
-  ngOnInit() {
+  cambiarTab(nombre: string) {
+    this.tab = nombre;
   }
-
 }

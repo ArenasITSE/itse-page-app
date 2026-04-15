a93bbb657eb60d4eb046ce1734aec5140b66c704
ArenasITSE <EduardoArenas@escarcega.tecnm.mx>
2026-04-15 11:48:13 -0600

    terminacion modelo miscelaneos

--- src/app/pages/gestion_institucional/Miscelaneo/CS_PpU006/CS_PpU006.component.ts
+++ src/app/pages/gestion_institucional/Miscelaneo/CS_PpU006/CS_PpU006.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; // 🔥 IMPORTANTE
 
 @Component({
   selector: 'app-CS_PpU006',
   templateUrl: './CS_PpU006.component.html',
+   standalone: true, // 🔥 NECESARIO
+  imports: [CommonModule], // 🔥 AQUÍ VA
   styleUrls: ['./CS_PpU006.component.css']
 })
 export class CS_PpU006Component implements OnInit {
+   tab: string = 'prodep';
 
   constructor() { }
 
--- src/app/pages/gestion_institucional/Miscelaneo/CS_S247_PRODEP/CS_S247_PRODEP.component.ts
+++ src/app/pages/gestion_institucional/Miscelaneo/CS_S247_PRODEP/CS_S247_PRODEP.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; // 🔥 IMPORTANTE
 
 @Component({
   selector: 'app-CS_S247_PRODEP',
+  standalone: true, // 🔥 NECESARIO
+  imports: [CommonModule], // 🔥 AQUÍ VA
   templateUrl: './CS_S247_PRODEP.component.html',
   styleUrls: ['./CS_S247_PRODEP.component.css']
 })
 export class CS_S247_PRODEPComponent implements OnInit {
 
+  tab: string = 'prodep';
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }

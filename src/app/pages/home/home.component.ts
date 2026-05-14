b2ffc1dad910da72974350adfe1435150ec08ff5
ArenasITSE <EduardoArenas@escarcega.tecnm.mx>
2026-05-14 15:03:08 -0600

    verificacion svg

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -15,12 +15,12 @@ export class HomeComponent {
     '/assets/carrusel_home/carrusel_2.jpg',
     '/assets/carrusel_home/carrusel_3.png',
     '/assets/carrusel_home/carrusel_4.jpg',
-/*     '/assets/carrusel_home/carrusel_5.jpg',
+    '/assets/carrusel_home/carrusel_5.jpg',
     '/assets/carrusel_home/carrusel_6.jpg',
     '/assets/carrusel_home/carrusel_7.jpg',
     '/assets/carrusel_home/carrusel_8.jpg',
-    '/assets/carrusel_home/carrusel_9.png',
-    '/assets/carrusel_home/carrusel_10.png' */
+/*     '/assets/carrusel_home/carrusel_9.jpg',
+    '/assets/carrusel_home/carrusel_10.jpg'  */
   ];
 
 }

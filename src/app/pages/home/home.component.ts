07b9bfa13844975da8e88190427326cbe9a00cf2
ArenasITSE <EduardoArenas@escarcega.tecnm.mx>
2026-06-09 10:59:51 -0600

    reto olina

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,26 +1,52 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // 🔥 ESTE FALTA
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule], // 🔥 AGRÉGALO AQUÍ
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
-  images: string[] = [
-    'assets/carrusel_home/carrusel_1.jpg',
-    'assets/carrusel_home/carrusel_2.jpg',
-    'assets/carrusel_home/carrusel_3.png',
-    'assets/carrusel_home/carrusel_4.jpg',
-    'assets/carrusel_home/carrusel_5.jpg',
-    'assets/carrusel_home/carrusel_6.jpg',
-    'assets/carrusel_home/carrusel_7.jpg',
-    'assets/carrusel_home/carrusel_8.jpg',
-/*     'assets/carrusel_home/carrusel_9.jpg',
-    'assets/carrusel_home/carrusel_10.jpg'  */
+  images = [
+    {
+      src: 'assets/carrusel_home/carrusel_1.jpg',
+      link: ''
+    },
+    {
+      src: 'assets/carrusel_home/olinia.jpg',
+      link: 'https://www.tecnm.mx/archivos/slider/Convocatoria_Retos_Olinia_TecNM.pdf?documento=49' // ← cambia por tu enlace
+    },
+    {
+      src: 'assets/carrusel_home/carrusel_2.jpg',
+      link: ''
+    },
+    {
+      src: 'assets/carrusel_home/carrusel_3.png',
+      link: ''
+    },
+    {
+      src: 'assets/carrusel_home/carrusel_4.jpg',
+      link: ''
+    },
+    {
+      src: 'assets/carrusel_home/carrusel_5.jpg',
+      link: ''
+    },
+    {
+      src: 'assets/carrusel_home/carrusel_6.jpg',
+      link: ''
+    },
+    {
+      src: 'assets/carrusel_home/carrusel_7.jpg',
+      link: ''
+    },
+    {
+      src: 'assets/carrusel_home/carrusel_8.jpg',
+      link: ''
+    }
   ];
 
 }

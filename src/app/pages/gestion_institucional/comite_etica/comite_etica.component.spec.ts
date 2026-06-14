95ecdd996622eb5f805be69871833fc757e46dd9
HuescaITSE <JulianHuesca@escarcega.tecnm.mx>
2026-06-14 13:52:50 -0600

    Arreglos Etica #1

--- src/app/pages/gestion_institucional/comite_etica/comite_etica.component.spec.ts
+++ src/app/pages/gestion_institucional/comite_etica/comite_etica.component.spec.ts
@@ -1,28 +1,29 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Comite_eticaComponent } from './comite_etica.component';
 
 describe('Comite_eticaComponent', () => {
+
   let component: Comite_eticaComponent;
   let fixture: ComponentFixture<Comite_eticaComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ Comite_eticaComponent ]
+  beforeEach(async () => {
+
+    await TestBed.configureTestingModule({
+      imports: [Comite_eticaComponent]
     })
     .compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(Comite_eticaComponent);
+
     component = fixture.componentInstance;
+
     fixture.detectChanges();
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
